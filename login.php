e056ed5ba8ea96f2b5caf8c5443caa3afee1fe31
DawidKaszubaCT <DKGitHub@proton.me>
2024-09-23 10:11:48 +0200

    zmiany w login.php 2

--- login.php
+++ login.php
@@ -2,6 +2,28 @@
 session_start();
 includes('includes/db.php');
 
-if ($_SERVER('REQUEST_METHOD') == 'POST')
+if ($_SERVER('REQUEST_METHOD') == 'POST')  {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
+
+$query = $conn->preapre("SELECT * FROM users WHERE username = ?");
+$query->bind_param('s', $username);
+$query->execute();
+$result = $query->get_result();
+
+if ($result->num_rows > 0)  {
+    $user = $result->fetch_assoc();
+    if($password_verify($password, $user[$password])) {
+        $_SESSION['user_id'] = $user['id'];
+        header('Location: index.php');
+        exit;
+    } else{
+        $error = "niepoprawne hasło!";
+    }
+    else {
+        $error = "użytkownik nie istnieje...a może istnieje? i nasze mózgią są resetowane przez rząd abyśmy się nie dowiedzieli prawdy? Czy jesteśmy prawdziwi? Czy my żyjemy w Matrixie?"
+    }
+}
 
 ?>
