77d6eee9bd454f631889d4a26540c62c3a634b3f
DawidKaszubaCT <DKGitHub@proton.me>
2024-09-23 10:13:14 +0200

    zmiany w login.php 3

--- login.php
+++ login.php
@@ -25,5 +25,16 @@ if ($result->num_rows > 0)  {
         $error = "użytkownik nie istnieje...a może istnieje? i nasze mózgią są resetowane przez rząd abyśmy się nie dowiedzieli prawdy? Czy jesteśmy prawdziwi? Czy my żyjemy w Matrixie?"
     }
 }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
